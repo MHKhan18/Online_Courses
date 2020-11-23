ac8c4d726b548804762036c4b52e3f0b24897c47
Mohammad Khan <mhrkhan13@gmail.com>
2020-11-22 23:37:52 -0500

    completed Create Listing

--- CS50-WebDev/Project-2/commerce/auctions/models.py
+++ CS50-WebDev/Project-2/commerce/auctions/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listings(models.Model):
+    title = models.CharField(max_length=30 , unique=True)
+    description = models.CharField(max_length=250)
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='images',blank=True, null=True)
+    category = models.CharField(max_length=10)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+
+    def __str__(self):
+        return f"title: {self.title} , bid: {self.bid} , owner: {self.owner.username} , image: {self.image} , category: {self.category}"
+
+
+class Bid(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
--- CS50-WebDev/Project-2/commerce/auctions/views.py
+++ CS50-WebDev/Project-2/commerce/auctions/views.py
@@ -4,7 +4,32 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from django import forms
+from django.contrib.auth.decorators import login_required
+
+from .models import User , Listings
+
+
+
+
+CATEGORY_CHOICES = [
+    ('None' , ''),
+    ('Fashion' , 'Fashion'),
+    ('Toys' , 'Toys'),
+    ('Electronics' , 'Electronics'),
+    ('Home' , 'Home'),
+    ('Car' , 'Car'),
+    ('Airplane' , 'Airplane')
+]
+
+class CreateListingsFrom(forms.Form):
+    title = forms.CharField(label="Title")
+    description = forms.CharField(label="Description" , widget = forms.Textarea)
+    bid = forms.DecimalField(label="Starting Bid" , decimal_places=2)
+    image = forms.ImageField(label="Upload an image(optional)" , required=False)
+    category = forms.CharField(label="Category (optional):",  required=False , widget = forms.Select(choices = CATEGORY_CHOICES))
+
+
 
 
 def index(request):
@@ -61,3 +86,29 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required(login_url='/login')
+def create_listing(request):
+
+    if request.method == "POST":
+        form = CreateListingsFrom(request.POST, request.FILES)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data['description']
+            bid = form.cleaned_data['bid']
+            image = form.cleaned_data['image']
+            category = form.cleaned_data['category']
+            owner = request.user 
+            try:
+                listing = Listings(title=title , description=description , bid=bid , image=image , category=category , owner=owner)
+                listing.save()
+                return HttpResponseRedirect(reverse("index"))
+            except IntegrityError:
+                return render(request , "auctions/create_listings.html" , {"form" : form , "error" : "Listing with this title already exists."}) 
+
+
+        return render(request , "auctions/create_listings.html" , {"form" : form}) # display errors
+
+    else:
+        form = CreateListingsFrom()
+        return render(request , "auctions/create_listings.html" , {"form" : form})
